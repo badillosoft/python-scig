4236238c99eb1579a887e5411fe09516d174a916
badillosoft <badillo.soft@hotmail.com>
2018-12-08 11:22:30 -0600

    sesión 5

---
+++ quoteresult.jl
@@ -0,0 +1,30 @@
+{"price": "$21,995.00", "name": "Pantalla LG 65\" UHD Smart TV 65UJ630"}
+{"price": "$12,995.00", "name": "Pantalla LG 55\" UHD Smart TV 55UJ63"}
+{"price": "$7,295.00", "name": "Pant SANSUI 50 \" SMX5019USM 4K Smart"}
+{"price": "$8,295.00", "name": "Pant JVC 49 \" SI49FSC Curva"}
+{"price": "$6,995.00", "name": "Pantalla Samsung 43\" Smart TV LH43SEJBG"}
+{"price": "$6,295.00", "name": "Pantalla JVC 43\" Smart TV SI43FS"}
+{"price": "$2,795.00", "name": "Televisor Sansui 32\"  SMX32Z1"}
+{"price": "$7,295.00", "name": "Consola Xbox One S + Assassin's Creed +"}
+{"price": "$1,395.00", "name": "Bafle AIWA 8'' AW908 50 Watts"}
+{"price": "$1,045.00", "name": "Autoestereo AIWA AW-3239BT Bluetooth"}
+{"price": "$8,990.00", "name": "Lavadora Mabe 19Kg. LMH79104WBAB"}
+{"price": "$10,795.00", "name": "Secadora Mabe SMG47N8MSBAB0 22 Kg Blanca"}
+{"price": "$10,295.00", "name": "Refrigerador Daewoo 16p3 44520GNDA"}
+{"price": "$4,995.00", "name": "Estufa IEM EI3030BAPN 30\""}
+{"price": "$3,995.00", "name": "Asador Home Line de Carb\u00f3n BB02543U"}
+{"price": "$2,295.00", "name": "Mesa para TV Home Line FII0761R"}
+{"price": "$1,399.00", "name": "Mu\u00f1eca Baby Alive Beb\u00e9 Va Al Ba\u00f1o Rubia"}
+{"price": "$499.00", "name": "Pista Hotwheels Cobra Trituradora FNB20"}
+{"price": "$219.00", "name": "Mu\u00f1eca Lol Surprise Lil Sisters Asst 12"}
+{"price": "$599.00", "name": "Set Barbie Chef de Pizza FHR09"}
+{"price": "$289.00", "name": "Figura Lol Surprise Pets 144050"}
+{"price": "$449.00", "name": "Set Crayola Fabricante D Marcador 747054"}
+{"price": "$999.00", "name": "Set Barbie Hospital M\u00f3vil FRM19"}
+{"price": "$199.00", "name": "Mu\u00f1eca Famosa Nenuco 3 Funciones Surtido"}
+{"price": "$379.00", "name": "Mu\u00f1eco Lol Surprise Confetti Pop 146680"}
+{"price": "$294.00", "name": "Mundo FisherPrice de Aprendizaje DRJ93"}
+{"price": "$299.00", "name": "Mu\u00f1eca Barbie Movimientos Divertid FTG80"}
+{"price": "$249.00", "name": "Figura TPZ Twisty Petz 4 figuras Beb\u00e9s"}
+{"price": "$899.00", "name": "Gimnasio Fisherprice Etapas Ccb70\u00a0"}
+{"price": "$999.00", "name": "Set Barbie Cocina y Divi\u00e9rtete FRH73"}
